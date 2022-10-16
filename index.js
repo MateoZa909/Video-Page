1069b355f0659a5e9569a49f42015f8a4caac02a
MateoZa909 <matbo5959@gmail.com>
2022-10-16 14:58:57 -0500

    New things

---
+++ index.js
@@ -0,0 +1,15 @@
+const home = document.querySelector('#home');
+// home.addEventListener('click', ClickTap);
+
+
+const animalking = document.querySelector('#animalking');
+const popularSpe = document.querySelector('#popularSpe');
+const contact = document.querySelector('#contact');
+
+// ARCHIVO ANIMALKINGDOM
+const returnTab = document.querySelector('.return');
+returnTab.addEventListener('click', returnHome);
+
+// function ClickTap() {
+    
+// }
